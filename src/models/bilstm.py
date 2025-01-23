b5ecd36003d2f60951d1bf0b1509ba96c9d08bd8
Chaimae Ramdani <127148153+eamiahc@users.noreply.github.com>
2025-01-23 11:29:42 +0100

    Update bilstm.py

--- src/models/bilstm.py
+++ src/models/bilstm.py
@@ -16,3 +16,19 @@ def build_bilstm_model(vocab_size, embedding_dim, max_len, embedding_matrix):
     ])
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
     return model
+
+def build_sc_bilstm_model(vocab_size, embedding_dim, max_len, embedding_matrix):
+    model = Sequential([
+        Embedding(input_dim=vocab_size, 
+                  output_dim=embedding_dim, 
+                  weights=[embedding_matrix], 
+                  input_length=max_len, 
+                  trainable=False),
+        Bidirectional(LSTM(128, return_sequences=True)),
+        Dropout(0.1),
+        Bidirectional(LSTM(64)),
+        Dropout(0.1),
+        Dense(1, activation='sigmoid')
+    ])
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    return model
